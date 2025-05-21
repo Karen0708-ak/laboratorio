70f257f12bdf18be963d706fcad41a04fa228a32
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-05-21 14:09:53 -0500

    Se cambio el idioma y la zona horaria

--- laboratorio/settings.py
+++ laboratorio/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'laboratorio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'laboratorio.sqlite3',
     }
 }
 
@@ -107,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ec'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Guayaquil'
 
 USE_I18N = True
 
