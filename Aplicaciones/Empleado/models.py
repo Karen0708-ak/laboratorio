856e362aed3c8d6b1fa870e89a9f07ba2cc3aa85
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-05-21 15:12:26 -0500

    Se realizo la migracion del modelo empleo

--- Aplicaciones/Empleado/models.py
+++ Aplicaciones/Empleado/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Vinedo(models.Model):
+    id_libro = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    ubicacion = models.CharField(max_length=200)
+    propietario = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=20, blank=True, null=True)
