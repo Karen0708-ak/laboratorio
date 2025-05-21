b1648c0c494ebf20d28ab69266cde46ddc447faa
Karen0708-ak <karen.barcia8759@utc.edu.ec>
2025-05-21 16:56:57 -0500

    Se agrego el insertar en la aplicacion Empleado

--- Aplicaciones/Empleado/models.py
+++ Aplicaciones/Empleado/models.py
@@ -4,5 +4,4 @@ class Empleado(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
     ubicacion = models.CharField(max_length=200)
-    propietario = models.CharField(max_length=100)
     telefono = models.CharField(max_length=20)
